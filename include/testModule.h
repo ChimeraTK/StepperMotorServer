2cec88ede0b2e4cca0f4af3d8ff740f31944c1d9
Christoph Kampmeyer <christoph.kampmeyer@desy.de>
2019-01-22 13:02:33 +0100

    Added testModule

---
+++ include/testModule.h
@@ -0,0 +1,43 @@
+
+#include <ChimeraTK/ApplicationCore/ApplicationCore.h>
+#include "MotorDriver.h"
+
+
+struct TestModule : public ChimeraTK::ApplicationModule{
+
+  using ChimeraTK::ApplicationModule::ApplicationModule;
+
+
+  ctk::ReadAnyGroup inputGroup;
+
+
+  //Limit speedLimit{this, "speedLimit", "motor speedLimit"}
+
+  struct Limit : public ctk::VariableGroup {
+
+    using ctk::VariableGroup::VariableGroup;
+
+    ctk::ScalarPushInput<double> userValue{this, "userValue", "", "Speed limit set for the motor"};
+    ctk::ScalarPushInput<double> maxValue{this, "maxValue", "", "Maximum velocity of the motor"};
+
+  };
+  Limit speedLimit{this, "speedLimit", "", false, {"MOTOR"}};
+
+  ChimeraTK::ScalarOutput<double> userSpeedLimit{this, "SpdLimOut", "", {"CS"}};
+
+  void mainLoop() override{
+
+    inputGroup = speedLimit.readAnyGroup();
+
+    while(true){
+
+      inputGroup.readAny();
+
+      userSpeedLimit = speedLimit.userValue * 2;
+      writeAll();
+    }
+  }
+
+};
+
+
