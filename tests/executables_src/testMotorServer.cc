c8313028699d1bea9c5a1bce61fc03a99afde219
Martin Killenberg <martin.killenberg@desy.de>
2020-11-06 17:31:35 +0100

    Fixed test - Using the compiled version of boost unit test to avoid double free corruption - Instantiating the application and the test facility inside the test case

--- tests/executables_src/testMotorServer.cc
+++ tests/executables_src/testMotorServer.cc
@@ -1,34 +1,19 @@
 // Define a name for the test module.
 #define BOOST_TEST_MODULE testMotorServer
 // Only after defining the name include the unit test header.
-#include <boost/test/included/unit_test.hpp>
-
-#include <ChimeraTK/ApplicationCore/TestFacility.h>
-
-#include "StepperMotorServer.h"
-
-// Declare the server instance
-static StepperMotorServer theServer;
-
-static ChimeraTK::TestFacility testFacility;
-
-struct TestFixture {
-  TestFixture() { testFacility.runApplication(); }
-};
-static TestFixture fixture;
-
+#include <boost/test/unit_test.hpp>
 using namespace boost::unit_test_framework;
 
-/// TestSuite for the server
-BOOST_AUTO_TEST_SUITE(StepperMotorServerTestSuite)
+#include <ChimeraTK/ApplicationCore/TestFacility.h>
+#include "StepperMotorServer.h"
 
 /**********************************************************************************************************************/
 
-/// A template test case
-BOOST_AUTO_TEST_CASE(testTemplate) {
-  std::cout << "testTemplate" << std::endl;
+// Just checks that the server instance can be instantiated and started
+BOOST_AUTO_TEST_CASE(testInstantiaton) {
+  StepperMotorServer theServer;
+  ChimeraTK::TestFacility testFacility;
+  testFacility.runApplication();
 }
 
 /**********************************************************************************************************************/
-
-BOOST_AUTO_TEST_SUITE_END()
