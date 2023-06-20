0389e61f8833346872aaba737b6c4644bcabe37c
Jens Georg <jens.georg@desy.de>
2023-06-20 18:01:50 +0200

    Update for motor module 0.7

--- config/initMotorDriverHW.py
+++ config/initMotorDriverHW.py
@@ -3,15 +3,17 @@
 import mtca4u
 import sys
 
-if len(sys.argv) < 3 :
-  print("Usage: initMotorDriverHW.py <dMapFileName> <boardAliasName>")
+if len(sys.argv) < 4 :
+  print("Usage: initMotorDriverHW.py <dMapFileName> <boardAliasName> <bspName>")
   sys.exit(1)
 
 dMapFileName = sys.argv[1]
 deviceName   = sys.argv[2]
+bspName = sys.argv[3]
 
 print("Performing initialization of device "+deviceName+".")
 mtca4u.set_dmap_location(dMapFileName)
 device = mtca4u.Device(deviceName)
  
-device.write("BOARD/0", "WORD_RESET_N", 1);
+if device.read(bspName, "WORD_RESET_N") == 0:
+    device.write(bspName, "WORD_RESET_N", 1);
