94597bbdef3bf9c9cfc0db345864f23eefd2500e
Jens Georg <jens.georg@desy.de>
2026-04-02 14:24:59 +0200

    chore: Port initscript to new python bindings

--- config/initMotorDriverHW.py
+++ config/initMotorDriverHW.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
-import mtca4u
+import deviceaccess as da
 import sys
 
-if len(sys.argv) < 4 :
-  print("Usage: initMotorDriverHW.py <dMapFileName> <boardAliasName> <bspName>")
-  sys.exit(1)
+if len(sys.argv) < 4:
+    print("Usage: initMotorDriverHW.py <dMapFileName> <boardAliasName> <bspName>")
+    sys.exit(1)
 
 dMapFileName = sys.argv[1]
-deviceName   = sys.argv[2]
+deviceName = sys.argv[2]
 bspName = sys.argv[3]
 
+da.setDMapFilePath(dMapFileName)
+
 print("Performing initialization of device "+deviceName+".")
-mtca4u.set_dmap_location(dMapFileName)
-device = mtca4u.Device(deviceName)
- 
-if device.read(bspName, "WORD_RESET_N") == 0:
-    device.write(bspName, "WORD_RESET_N", 1);
+device = da.Device(deviceName)
+device.open()
+
+
+if device.read(f"{bspName}.WORD_RESET_N") == 0:
+    device.write(f"{bspName}.WORD_RESET_N", 1)
